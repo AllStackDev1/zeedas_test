d287f78af39fa57532c2b7ed7f8318c7d7a7d567
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-03-07 01:09:16 +0100

    remove reduntant React imports

--- src/components/Wallet/Balance.js
+++ src/components/Wallet/Balance.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Box, Flex, Text, Button } from '@chakra-ui/react'
 import { FiChevronRight } from 'react-icons/fi'
 
--- src/container/Layout/Wrapper.js
+++ src/container/Layout/Wrapper.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 import { Box, Flex, Image } from '@chakra-ui/react'
 
